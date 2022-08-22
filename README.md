c7429be0a6655562d09e2bd38b451b02baa7c37f
Dhia M <merzougui.dhia@gmail.com>
2022-08-22 08:36:19 +0200

    Added initial connect/disconnect functionality

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # PlatmanQuizman
-A Discord bot that plays the first 15 seconds of each YouTube link it receives as a direct message from a predefined user.
+A Discord bot that plays the first 15 seconds of each YouTube link it receives via direct message from a predefined user.
 
 [Add Link](https://discord.com/api/oauth2/authorize?client_id=1009551579487154176&permissions=1117184&scope=bot%20applications.commands)
+
+## Commands
+Default prefix: &  
+
+| Command | Description                        |
+|---------|------------------------------------|
+| stalk   | Connect to a voice channel.        |
+| caught  | Disconnect from the voice channel. |
--- index.js
+++ index.js
@@ -1,14 +1,54 @@
-const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
+const { Client, GatewayIntentBits, ActivityType, Partials } = require('discord.js');
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
 require('dotenv').config()
 
-const client = new Client({ partials: ["CHANNEL"], intents: [GatewayIntentBits.Guilds] });
+const prefix = "&";
+const vcId = process.env.vcId;
+
+const client = new Client({
+        partials: [
+            Partials.Channel
+        ],
+        intents: [
+            GatewayIntentBits.Guilds,
+            GatewayIntentBits.DirectMessages,
+            GatewayIntentBits.GuildMessages,
+            GatewayIntentBits.MessageContent
+            ]
+});
 
 client.once("ready", () => {
     console.log("Ready!");
+    client.user.setActivity("you through your window.", {type: ActivityType.Watching});
 })
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.on("ready", () => {
-    client.user.setActivity("you from your window.", {type: ActivityType.Watching});
+client.on("messageCreate", (message) => {
+
+    if(message.guildId === null){
+        console.log(`====DIRECT Message====\n${message}`);
+        const command = message.content.split(" ");
+    }
+    
+    if(message.guildId !== null && message.content[0] == prefix){
+        console.log(`====SERVER message====\n${message}`);
+        const command = message.content.split(prefix);
+
+        if(command[1] == "stalk"){
+
+            const channel = client.channels.cache.get(vcId);
+
+            const connection = joinVoiceChannel({
+                channelId: channel.id,
+                guildId: channel.guild.id,
+                adapterCreator: channel.guild.voiceAdapterCreator,
+            })
+        }
+
+        if(command[1] == "caught"){
+            const connection = getVoiceConnection(message.guildId);
+            connection.destroy();
+        }
+    }
 })
