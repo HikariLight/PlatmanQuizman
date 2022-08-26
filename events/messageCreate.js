3cf8eebd098dfd7014903f3d857182d689016f25
Dhia M <merzougui.dhia@gmail.com>
2022-08-26 14:10:12 +0200

    Added restriction to who can use the bot

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,45 +3,48 @@ const caught = require("../commands/caught");
 const play = require("../commands/play");
 const pause = require("../commands/pause");
 const unpause = require("../commands/unpause");
+const allowedUsers = require("../data.json")["allowedUsers"];
 
 module.exports = (client, prefix, player, message) => {
 
-    if(message.guildId === null){
-        const command = message.content.split(" ");
+    if(allowedUsers.includes(message.author.id)){
+        if(message.guildId === null){
+            const command = message.content.split(" ");
 
-        console.log(`Direct message: ${message.content}`);
+            console.log(`Direct message: ${message.content}`);
 
-        switch(command[0]){
-            case "play":
-                play(message, command, player);
-                break;
+            switch(command[0]){
+                case "play":
+                    play(message, command, player);
+                    break;
 
-            case "pause":
-                pause(message, player);
-                break;
+                case "pause":
+                    pause(message, player);
+                    break;
 
-            case "unpause":
-                unpause(message, player);
-                break;
+                case "unpause":
+                    unpause(message, player);
+                    break;
+            }
         }
-    }
 
-    else if(message.guildId !== null && message.content.startsWith(prefix)){
-        const command = message.content.split(prefix);
+        else if(message.guildId !== null && message.content.startsWith(prefix)){
+            const command = message.content.split(prefix);
 
-        console.log(`Server message: ${message.content}`);
+            console.log(`Server message: ${message.content}`);
 
-        switch(command[1]){
-            case "stalk":
-                stalk(client, message);
-                break;
+            switch(command[1]){
+                case "stalk":
+                    stalk(client, message);
+                    break;
 
-            case "caught":
-                caught(message);
-                break;
+                case "caught":
+                    caught(message);
+                    break;
 
-            default:
-                message.channel.send("Tf did you just say to me?");
+                default:
+                    message.channel.send("Tf did you just say to me?");
+            }
         }
     }
 }
