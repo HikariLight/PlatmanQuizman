80ff3cc1f4aef6a387bc2e250f05990bb3734089
Dhia M <merzougui.dhia@gmail.com>
2023-07-28 00:08:08 +0200

    Fixed playback bug

--- commands/stalk.js
+++ commands/stalk.js
@@ -1,11 +1,13 @@
-const { joinVoiceChannel } = require('@discordjs/voice');
-require('dotenv').config()
+const { joinVoiceChannel, VoiceConnectionStatus } = require("@discordjs/voice")
+require("dotenv").config()
 
 module.exports = (message) => {
+    if (!message.member.voice?.channel)
+        return message.channel.send(
+            "Where am I supposed to go? Connect to a Voice Channel bish."
+        )
 
-    if (!message.member.voice?.channel) return message.channel.send('Where am I supposed to go? Connect to a Voice Channel bish.');
-
-    const voiceChannel = message.member.voice?.channel;
+    const voiceChannel = message.member.voice?.channel
 
     const voiceConnection = joinVoiceChannel({
         channelId: voiceChannel.id,
@@ -13,9 +15,17 @@ module.exports = (message) => {
         adapterCreator: voiceChannel.guild.voiceAdapterCreator,
     })
 
-    voiceConnection.on('stateChange', (oldState, newState) => {
-        console.log(`Connection transitioned from ${oldState.status} to ${newState.status}`);
-    });
+    voiceConnection.on("stateChange", (oldState, newState) => {
+        console.log(
+            `Connection transitioned from ${oldState.status} to ${newState.status}`
+        )
+        if (
+            oldState.status === VoiceConnectionStatus.Ready &&
+            newState.status === VoiceConnectionStatus.Connecting
+        ) {
+            voiceConnection.configureNetworking()
+        }
+    })
 
-    message.channel.send("boutta stalk you *real* good bby gurl ;)");
+    message.channel.send("boutta stalk you *real* good bby gurl ;)")
 }
